c75ffe3bf37565a5452a46a6f4db9e55759814c3
Lak <lakmal.siyambalage@theaccessgroup.com>
2024-02-20 17:33:47 +0530

    About responsive

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -5,11 +5,57 @@ import { useMediaQuery } from "react-responsive";
 
 function About() {
   const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
-
+  const isLapscreen = useMediaQuery({ query: "(max-width: 1280px)" });
+  const title = "Resourcing your business for the competitive edge";
+  const vision = `Our vision is to become a preeminent resource provider in the software development and information technology industry, celebrated for our delivery of exceptional, skills, talent that empower our clients to reach their business objectives with excellence.`;
+  const mission = `Our mission is to introduce innovative skill outsourcing models and deliver a perfect mix of talented resources that are meticulously crafted to address our clients' unique requirements. We are committed to upholding the utmost professionalism, expertise, and transparency, ensuring that our clients receive the highest standard of service.`;
+  const strategy = `Through our Sourcing Readiness Assessment (SRA), we evaluate your outsourcing goals, including feasibility, opportunity assessment, cost savings, strategic alignment, and provide you actionable recommendations. Our objective is to empower your organization with insights and tools for informed decisions, optimized outsourcing returns, and business growth.`;
+  const description = () => {
+    return (
+      <div className={style.title_description}>
+        EigenIvory is a Sri Lankan registered private limited company and is the
+        outsourcing arm of the UK based
+        <span className={style.highlight}> EigenGlobal</span> group. We as a
+        company provide you with a wide range of innovative outsourcing models.
+        EigenIvory is your premier choice for extended IT teams. We prioritize
+        delivering the right skills mix to perfectly align with your business
+        requirements. With our expertise and ample resources, we ensure a job
+        well done, every time.
+      </div>
+    );
+  };
   return (
     <>
       {isMobile ? (
-        <div className={style.mobile}></div>
+        <div className={style.mobile}>
+          <div className={style.main_about_content}>
+            <div className={style.about_title}>
+              <h3>ABOUT</h3>
+            </div>
+            <Space direction="vertical">
+              <div className={style.title}>{title}</div>
+              {description()}
+              <div className={style.content_item}>
+                <div className={style.vision_content}>
+                  <h3>Vision</h3>
+                  <p>{vision}</p>
+                </div>
+              </div>
+              <div className={style.content_item}>
+                <div className={style.mission_content}>
+                  <h3>Mission</h3>
+                  <p>{mission}</p>
+                </div>
+              </div>
+              <div className={style.content_item}>
+                <div className={style.strategy_content}>
+                  <h3>Strategy</h3>
+                  <p>{strategy}</p>
+                </div>
+              </div>
+            </Space>
+          </div>
+        </div>
       ) : (
         <div className={style.desktop}>
           <div className={style.main_about_content}>
@@ -29,22 +75,15 @@ function About() {
               <Col span={24}>
                 <Row style={{}}>
                   <Col span={8} className={style.title}>
-                    Resourcing your business for the competitive edge
+                    {title}
                   </Col>
                   <Col span={12} offset={4} className={style.title_description}>
-                    EigenIvory is a Sri Lankan registered private limited
-                    company and is the outsourcing arm of the UK based
-                    <span className={style.highlight}> EigenGlobal</span> group.
-                    We as a company provide you with a wide range of innovative
-                    outsourcing models. EigenIvory is your premier choice for
-                    extended IT teams. We prioritize delivering the right skills
-                    mix to perfectly align with your business requirements. With
-                    our expertise and ample resources, we ensure a job well
-                    done, every time.
+                    {description()}
                   </Col>
                 </Row>
               </Col>
               <Row
+                className={isLapscreen ? style.lapscreen : ""}
                 style={{
                   marginTop: "50px",
                 }}
@@ -58,41 +97,19 @@ function About() {
                 <Col span={8} className={style.content_item}>
                   <div className={style.vision_content}>
                     <h3>Vision</h3>
-                    <p>
-                      Our vision is to become a preeminent resource provider in
-                      the software development and information technology
-                      industry, celebrated for our delivery of exceptional,
-                      skills, talent that empower our clients to reach their
-                      business objectives with excellence.
-                    </p>
+                    <p>{vision}</p>
                   </div>
                 </Col>
                 <Col span={8} className={style.content_item}>
                   <div className={style.mission_content}>
                     <h3>Mission</h3>
-                    <p>
-                      Our mission is to introduce innovative skill outsourcing
-                      models and deliver a perfect mix of talented resources
-                      that are meticulously crafted to address our clients'
-                      unique requirements. We are committed to upholding the
-                      utmost professionalism, expertise, and transparency,
-                      ensuring that our clients receive the highest standard of
-                      service.
-                    </p>
+                    <p>{vision}</p>
                   </div>
                 </Col>
                 <Col span={8} className={style.content_item}>
                   <div className={style.strategy_content}>
                     <h3>Strategy</h3>
-                    <p>
-                      Through our Sourcing Readiness Assessment (SRA), we
-                      evaluate your outsourcing goals, including feasibility,
-                      opportunity assessment, cost savings, strategic alignment,
-                      and provide you actionable recommendations. Our objective
-                      is to empower your organization with insights and tools
-                      for informed decisions, optimized outsourcing returns, and
-                      business growth.
-                    </p>
+                    <p>{strategy}</p>
                   </div>
                 </Col>
               </Row>
