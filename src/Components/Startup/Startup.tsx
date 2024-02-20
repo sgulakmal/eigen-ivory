9e717422fb676851ad61f4ea153e49c12b3494bb
Lak <lakmal.siyambalage@theaccessgroup.com>
2024-02-20 15:37:41 +0530

    Startup

--- src/Components/Startup/Startup.tsx
+++ src/Components/Startup/Startup.tsx
@@ -6,29 +6,51 @@ import { useMediaQuery } from "react-responsive";
 
 function Startup() {
   const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
+
+  const detailsElement = () => {
+    return (
+      <>
+        <div className={style.text_container_1}>
+          <h2>Offshore, Onshore and Nearshore Skill Outsourcing</h2>
+        </div>
+        <div className={style.text_container_2}>
+          <p>
+            From skilled developers to large development teams, we are here to
+            provide you with what your company needs to grow.
+          </p>
+        </div>
+      </>
+    );
+  };
+
   return (
-    <div className={style.content}>
-      <Row>
-        <Col span={12} className={style.start_up_text}>
-          <div className={style.text_container_1}>
-            <h2>Offshore, Onshore and Nearshore Skill Outsourcing</h2>
-          </div>
-          <div className={style.text_container_2}>
-            <p>
-              From skilled developers to large development teams, we are here to
-              provide you with what your company needs to grow.
-            </p>
-          </div>
-        </Col>
-        <Col span={12} style={{ textAlign: "right" }}>
+    <>
+      {isMobile ? (
+        <div className={style.mobile}>
+          {detailsElement()}
           <img
             src={teamImg}
             alt="team-img"
-            style={{ width: "500px", height: "500px;" }}
+            style={{ width: "100%", height: "250px;" }}
           />
-        </Col>
-      </Row>
-    </div>
+        </div>
+      ) : (
+        <div className={style.desktop}>
+          <Row>
+            <Col span={12} className={style.start_up_text}>
+              {detailsElement()}
+            </Col>
+            <Col span={12}>
+              <img
+                src={teamImg}
+                alt="team-img"
+                style={{ width: "500px", height: "500px;" }}
+              />
+            </Col>
+          </Row>
+        </div>
+      )}
+    </>
   );
 }
 
