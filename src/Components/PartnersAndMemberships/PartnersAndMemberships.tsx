67b627411baf123505daff6addc7d31379a1bb4c
Lak <ulakmal@fetchseed.com>
2024-02-22 22:52:47 +0530

    Partners

--- src/Components/PartnersAndMemberships/PartnersAndMemberships.tsx
+++ src/Components/PartnersAndMemberships/PartnersAndMemberships.tsx
@@ -1,19 +1,27 @@
 import React from "react";
 import style from "./PartnersAndMemberships.module.scss";
 import { useMediaQuery } from "react-responsive";
-import { Col, Row, Space } from "antd";
-// import partnersmembers from '../../Assests/partners-members.svg'
-// import starmanconsotium from '../../Assests/starman-consortium.svg'
-// import enricharcane from '../../Assests/enrich-arcane.svg'
-// import lankalab from '../../Assests/lanka-lab.svg'
-// import fetchseed from '../../Assests/fetch-seed.svg'
-// import siyanomo from '../../Assests/siyanomo.svg'
+import { Card, Col, Row, Space } from "antd";
+import enrichImg from "../../Assests/partners/enrich.svg";
+import fetchseedImg from "../../Assests/partners/fetchseed.svg";
+import lankaLabsImg from "../../Assests/partners/lanka-labs.svg";
+import siyanmoImg from "../../Assests/partners/siyanmo.svg";
+import starmanImg from "../../Assests/partners/starman.svg";
+import Meta from "antd/es/card/Meta";
 
 function PartnersandMemberships() {
   const isMobile = useMediaQuery({ query: "(max-width: 768px)" });
   const title = "Our Valued Partner Customers: Fueling Collective Success";
   const description =
     "Our partners are an integral part of our success story. We value their expertise, insights, and contributions, which enrich our solutions and broaden our capabilities. Through collaborative efforts, we forge strong alliances built on trust, transparency, and shared goals. Together, we navigate the ever-changing landscape of technology and industry trends, driving innovation and staying one step ahead. Our partners' diverse perspectives and specialized knowledge enable us to deliver comprehensive, tailored solutions that exceed client expectations. With open lines of communication and a commitment to mutual growth, we cultivate long-lasting partnerships that fuel our collective success in delivering high-quality, customized solutions to our clients.";
+  const partners = [
+    { name: "Starman Consortium", image: starmanImg },
+    { name: "Enrich Arcane", image: enrichImg },
+    { name: "Lanka Labs Consultancies", image: lankaLabsImg },
+    { name: "Fetch Seed", image: fetchseedImg },
+    { name: "Siyanmo Technologies", image: siyanmoImg },
+  ];
+
   return (
     <>
       {isMobile ? (
@@ -55,6 +63,28 @@ function PartnersandMemberships() {
                 </Row>
               </Col>
             </Row>
+            <div
+              className={style.companies}
+            >
+              {partners.map((item, index) => (              
+                <div key={index} className={style.company_item}>
+                  <Card
+                    hoverable
+                    style={{ width: 240, height: 250, marginTop: "10px" }}
+                    cover={
+                      <div className={style.company_image_wrapper}>
+                        <img
+                          className={style.company_image}
+                          alt={item.name}
+                          src={item.image}
+                        />
+                        <h3>{item.name}</h3>
+                      </div>
+                    }
+                  ></Card>
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       )}
